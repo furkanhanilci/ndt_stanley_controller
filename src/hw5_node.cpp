f07934bcbd142dab05597b5e6b4ac7ea1b22d3e7
furkanhanilci <h.furkan90@gmail.com>
2024-01-31 14:32:41 +0300

    edit

--- src/hw5_node.cpp
+++ src/hw5_node.cpp
@@ -43,7 +43,13 @@ pcl::PointCloud<pcl::PointXYZ>::Ptr downsample(const pcl::PointCloud<pcl::PointX
     pcl::fromPCLPointCloud2(*pcl_cloud2_filtered,*pcl_cloud_filtered);
     return pcl_cloud_filtered;
 }
-
+/*
+  ************* sensor_data *************
+  This function is a callback triggered when sensor data is received.
+  It converts the ROS message to a PCL point cloud,
+  downsamples it, aligns it with a reference map using ICP registration,
+  and updates the transformation matrix accordingly.
+ */
 void sensor_data(const sensor_msgs::PointCloud2::ConstPtr& msg)
 {
     pcl::fromROSMsg(*msg,*pcl_cloud);
@@ -59,7 +65,46 @@ void sensor_data(const sensor_msgs::PointCloud2::ConstPtr& msg)
     std::cout << icp.getFinalTransformation() << std::endl;
     trans = icp.getFinalTransformation();
 }
+/*
+************* tf_brocast *************
+-   Initializing TransformBroadcaster:
+    tf::TransformBroadcaster br;
+    -   tf::TransformBroadcaster is a class in the ROS tf library used for broadcasting transformations between frames.
+    -   It is initialized as static to ensure that only one instance of TransformBroadcaster is created throughout the program's execution.
 
+-   Creating a Transformation:
+    tf::Transform tf_map_scan;
+    -   tf::Transform is a class in the ROS tf library used for storing transformations.
+    -   tf_map_scan is the name of the transformation between the map and the scan frames.
+
+-   Extraction Rotation and Translation:
+    Eigen::Quaternionf q(trans.topLeftCorner<3, 3>());
+    Eigen::Vector3f v = trans.topRightCorner<3, 1>();
+    -   The rotation and translation components are extracted from the transformation matrix trans.
+    -   trans.topLeftCorner<3, 3>() extracts the rotation matrix, which is converted to a quaternion (Eigen::Quaternionf q).
+    -   trans.topRightCorner<3, 1>() extracts the translation vector (Eigen::Vector3f v).
+
+ -  Setting Rotation and Translation:
+    tf_map_scan.setRotation(tf::Quaternion(q.x(), q.y(), q.z(), q.w()));
+    tf_map_scan.setOrigin(tf::Vector3(v(0), v(1), v(2)));
+    -   The rotation and translation components are set for the tf_map_scan transform.
+    -   tf_map_scan.setRotation() sets the rotation using the quaternion obtained from the rotation matrix.
+    -   tf_map_scan.setOrigin() sets the translation using the translation vector.
+
+ -  Broadcasting the Transformation:
+    br.sendTransform(tf::StampedTransform(tf_map_scan, ros::Time::now(), "map", "scan"));
+    -   The tf_map_scan transform is broadcasted between the map and scan frames.
+    -   tf::StampedTransform is a class in the ROS tf library used for storing transformations with a timestamp.
+    -   tf_map_scan is the name of the transformation between the map and the scan frames.
+    -   ros::Time::now() is the timestamp of the transformation.
+    -   "map" is the name of the parent frame.
+    -   "scan" is the name of the child frame.
+
+In summary, the tf_brocast() function calculates the transformation between the "map" and "scan" frames from a given transformation matrix,
+creates a tf::Transform object representing this transformation, and broadcasts it using the tf::TransformBroadcaster.
+This transformation is essential for understanding the spatial relationship between the map and sensor data captured in the "scan" frame.
+
+*/
 void tf_brocast(Eigen::Matrix4f trans){
   static tf::TransformBroadcaster br;
   tf::Transform tf_map_scan;
@@ -70,6 +115,17 @@ void tf_brocast(Eigen::Matrix4f trans){
   br.sendTransform(tf::StampedTransform(tf_map_scan, ros::Time::now(), "map", "scan"));
 }
 
+/*
+************* main *************
+- Loads the reference map from a file.
+- Initializes ROS node, subscribers, publishers, and rate control.
+- Sets an initial guess for the transformation between sensor and map frames.
+- Enters the main loop, where it continuously:
+    - Downsamples the map.
+    - Publishes the map and aligned scan.
+    - Broadcasts the transformation between "map" and "scan" frames.
+    - Spins ROS callbacks and controls loop rate for real-time processing.
+*/
 
 int main(int argc, char** argv)
 {
